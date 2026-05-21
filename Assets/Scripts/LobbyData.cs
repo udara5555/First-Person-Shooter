18e0525585366f4d451a7984f75f9fbde416afbe
udara5555 <udara.chathuranga.md@gmail.com>
2026-05-21 12:22:09 +0530

    default weapo added

--- Assets/Scripts/LobbyData.cs
+++ Assets/Scripts/LobbyData.cs
@@ -5,5 +5,5 @@ public static class LobbyData
     public static string RoomId = "";
     public static string PlayerName = "";
     public static bool IsCreator = false;
-    public static List<string> SelectedWeapons = new List<string>();
+    public static List<string> SelectedWeapons = new List<string> { "ak47", "magnum" }; // Default weapons: AK-47 and Magnum
 }
