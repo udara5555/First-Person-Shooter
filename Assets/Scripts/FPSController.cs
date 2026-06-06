a4452621f15a0c3c795643b2f0141554698df3d1
udara5555 <udara.chathuranga.md@gmail.com>
2026-06-06 15:46:18 +0530

    gun name placement fix

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -84,7 +84,7 @@ public class FPSController : MonoBehaviour
         if (animator != null)
         {
             animator.SetBool("isWalking", isMoving);
-            animator.SetBool("IsSprinting", isSprinting);
+            animator.SetBool("isSprinting", isSprinting);
         }
     }
 
